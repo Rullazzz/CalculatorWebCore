22f946f261f55cdfbe0054aa2b9c45fd1bb7ca75
Rullazzz <vip.sysyuk@mail.ru>
2022-10-07 23:41:54 +0700

    Добавьте файлы проекта.

---
+++ CalculatorWebCore/Pages/Index.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data;
+
+namespace CalculatorWebCore.Pages
+{
+	public class IndexModel : PageModel
+	{
+		private readonly ILogger<IndexModel> _logger;
+
+		public string? ResultExpression { get; private set; }
+
+		public IndexModel(ILogger<IndexModel> logger)
+		{
+			_logger = logger;
+		}
+
+		public void OnGet(string expression)
+		{
+			try
+			{
+				ResultExpression = new DataTable().Compute(expression, null).ToString();
+				_logger.Log(LogLevel.Information, "expression = {0}, result = {1}", expression, ResultExpression);
+			}
+			catch (SyntaxErrorException)
+			{
+				ResultExpression = "Error";
+				_logger.Log(LogLevel.Error, "syntax error");
+			}
+			catch (Exception)
+			{
+				ResultExpression = "Error";
+				_logger.Log(LogLevel.Error, "unknown error");
+			}
+
+		}
+	}
+}
---
+++ CalculatorWebCore/wwwroot/js/site.js
@@ -0,0 +1,8 @@
+﻿function dis(val) {
+    document.getElementById("result").value += val
+}
+
+// Function that clear the display
+function clr() {
+    document.getElementById("result").value = ""
+}
